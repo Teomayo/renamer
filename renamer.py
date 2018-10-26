4e08cfa93073f77d26c052530670756595aadaf1
Teomayo <tcrnobrn@edu.uwaterloo.ca>
2018-10-25 23:11:16 -0400

    updated files to work in multiple scenarios

--- renamer.py
+++ renamer.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+s = 1
 folder = r"C:\Users\Teodor\OneDrive - University of Waterloo\UWAFT work\Pictures\Misc"
 location = input("Photo Location:")
 files = os.listdir(folder)
@@ -8,7 +9,9 @@ for filename in files:
         file = os.path.join(folder, filename)
         m_time = os.path.getmtime(file)
         real_time = datetime.fromtimestamp(m_time)
-        f_time = datetime.strftime(real_time, "%Y-%m-%d %H_%M_%S")
-        new_filename = f_time + "_" + location + ".png"
+        f_time = datetime.strftime(real_time, "%Y-%b-%d")
+        ext = os.path.splitext(file)[-1].lower()
+        new_filename = f_time + "_" + location + "_" + str(s) + ext
         new_file = os.path.join(folder, new_filename)
         os.rename(file, new_file)
+        s += 1
